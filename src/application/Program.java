0d09dcc95ed77cdaabdf41e13b6f8fe011eb21bc
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-04-05 16:16:50 -0300

    Reuso das instancias e Implementando findByDepartamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.FabricaDAO;
 import model.dao.VendedorDAO;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Program {
@@ -10,9 +13,16 @@ public class Program {
 		
 		VendedorDAO vendedorDAO = FabricaDAO.criarVendedorDAO();
 		
-		Vendedor vendedor = vendedorDAO.findById(3);
-		
+		System.out.println("---------- findById ----------");
+		Vendedor vendedor = vendedorDAO.findById(3);		
 		System.out.println(vendedor);
+		
+		System.out.println("\n---------- FindByDepartamento ----------");
+		Departamento departamento = new Departamento(2, null);
+		List<Vendedor> list = vendedorDAO.findByDepartamento(departamento);
+		for (Vendedor ven : list) {
+			System.out.println(ven);
+		}
 	}
 
 }
--- src/model/dao/impl/VendedorDAOJDBC.java
+++ src/model/dao/impl/VendedorDAOJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -51,16 +54,8 @@ public class VendedorDAOJDBC implements VendedorDAO{
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
-				Departamento dep = new Departamento();
-				dep.setId(rs.getInt("DepartamentoId"));
-				dep.setNome(rs.getString("DepNome"));
-				Vendedor ven = new Vendedor();
-				ven.setId(rs.getInt("id_vendendor"));
-				ven.setNome(rs.getString("nome"));
-				ven.setEmail(rs.getString("email"));
-				ven.setDataNasc(rs.getDate("dataNasc"));
-				ven.setBaseSalario(rs.getDouble("basesalario"));
-				ven.setDepartamento(dep);
+				Departamento dep = instanciaDepartamento(rs);				
+				Vendedor ven = instanciaVendedor(rs, dep);
 				return ven;
 			}
 			return null;
@@ -72,10 +67,64 @@ public class VendedorDAOJDBC implements VendedorDAO{
 		}		
 	}
 
+	private Vendedor instanciaVendedor(ResultSet rs, Departamento dep) throws SQLException {
+		Vendedor ven = new Vendedor();
+		ven.setId(rs.getInt("id_vendendor"));
+		ven.setNome(rs.getString("nome"));
+		ven.setEmail(rs.getString("email"));
+		ven.setDataNasc(rs.getDate("dataNasc"));
+		ven.setBaseSalario(rs.getDouble("basesalario"));
+		ven.setDepartamento(dep);
+		return ven;
+	}
+
+	private Departamento instanciaDepartamento(ResultSet rs) throws SQLException {
+		Departamento dep = new Departamento();
+		dep.setId(rs.getInt("DepartamentoId"));
+		dep.setNome(rs.getString("DepNome"));
+		return dep;
+	}
+
 	@Override
 	public List<Vendedor> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Vendedor> findByDepartamento(Departamento departamento) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT vendedor.*,departamento.nome as DepNome " + 
+					"FROM vendedor INNER JOIN departamento " + 
+					"ON vendedor.DepartamentoId = departamento.id_departamento " + 
+					"WHERE DepartamentoId = ? " + 
+					"ORDER BY nome");
+			st.setInt(1, departamento.getId());
+			rs = st.executeQuery();
+			List<Vendedor> listaVen = new ArrayList<>();
+			Map<Integer, Departamento> map = new HashMap<>();
+			while (rs.next()) {
+				
+				Departamento dep = map.get(rs.getInt("DepartamentoId"));
+				
+				if (dep == null) {
+					dep = instanciaDepartamento(rs);
+					map.put(rs.getInt("DepartamentoID"), dep);
+				}				
+								
+				Vendedor ven = instanciaVendedor(rs, dep);
+				listaVen.add(ven);
+			}
+			return listaVen;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}	
 	}
 
 }
